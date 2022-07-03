cc73879afab8f64b7b00af868e97cea2e131fe72
lovegoodvibe <nstu30012001@gmail.com>
2022-07-03 17:31:19 +0700

    DAY LA COMMIT THEM FILE RESULT VA SUA FILE APP.JS

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            name: 'Nguyen Van B',
+            name: 'Nguyen Van BA',
             age: 18
         }
     }
---
+++ src/components/result.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+
+class result extends Component {
+    render() {
+        return (
+            <div>
+                 day la file test cho git project
+            </div>
+
+        )
+    }
+}
+
+export default result;
